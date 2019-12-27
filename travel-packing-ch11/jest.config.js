101b2b0654afba6e0055b906c8fae27d18a6aaed
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-12-27 07:28:39 -0600

    got Jest working

--- travel-packing-ch11/jest.config.js
+++ travel-packing-ch11/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  transform: {
-    '^.+  .js$': 'babel-jest',
-    '^.+  .svelte$': 'jest-transform-svelte'
-  },
-  moduleFileExtensions: ['js', 'svelte'],
   bail: false,
+  moduleFileExtensions: ['js', 'svelte'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.svelte$': 'svelte-jester'
+  },
   verbose: true
 };
